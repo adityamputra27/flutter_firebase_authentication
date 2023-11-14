dd0b99236cdbaea9fdad8b84c8bb4839de6cddf3
dittmptr27 <adityamuhamadputra@gmail.com>
2023-11-14 14:06:19 +0700

    remove unused code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_authentication/screens/login_email_password_screen.dart';
 import 'package:flutter_firebase_authentication/screens/phone_screen.dart';
